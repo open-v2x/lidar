c2e45cbf7e6f5b7a6db8ff781bda75991058aba4
99cloud <99cloud@99clouddeMacBook-Pro.local>
2022-10-24 15:22:18 +0800

    fix: transfet lid23D.cap to aliyun oss

--- project/websockets_client.py
+++ project/websockets_client.py
@@ -8,18 +8,14 @@ import websockets
 
 
 async def hello():
-    async with websockets.connect("ws://150.158.47.136:8000/ws/127.0.0.1", max_size=None) as websocket:
-    # async with websockets.connect("ws://127.0.0.1:8000/ws/127.0.0.1") as websocket:
+    async with websockets.connect("ws://47.100.126.13:8000/ws/127.0.0.1", max_size=None) as websocket:
         await websocket.send("Hello world!")
         while True:
             data = await websocket.recv()
             data = json.loads(data)
-            # print(data[:10])
-            # with open('test', 'w')as f:
-            #     f.write(data)
+            print(data[:10])
             print(time.time())
 
-            # await websocket.send("Hello world!")
 
 
 asyncio.run(hello())
