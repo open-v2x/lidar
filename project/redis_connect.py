82056ccfc5774ac3db0549054810358b308c0379
99cloud <99cloud@99clouddeMacBook-Pro.local>
2022-11-15 17:36:39 +0800

    fix: change redis port

--- project/redis_connect.py
+++ project/redis_connect.py
@@ -5,5 +5,5 @@ import redis
 redis_conn = redis.StrictRedis(
     host=os.environ.get("host", "127.0.0.1"),
     password=os.environ.get("redis_password", "123456"),
-    port=6378,
+    port=6379,
 )
